54f47bf57853e234c2978882fa0224ca8aeb6f39
avi202020 <avadhesh.cs@gmail.com>
2023-03-06 19:23:01 +0000

    created header and home pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,28 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
     return <>
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                {/*<a href="/">Home</> */}
-                <NavLink to="/" end>Home</NavLink>
-            </li>
-            <li className="nav-item">
-                {/*<a href="/contact">Contact</> */}
-                <NavLink to="/contact" end>Contact</NavLink>
-            </li>
-            <li className="nav-item">
-                {/*<a href="/projects">Projects</> */}
-                <NavLink to="/projects" end>Projects</NavLink>
-            </li>
-        </ul>
+       <nav className='navbar  navbar-expand-sm' id="navbar-bg">
+        <div className="container">
+            <Link to="/" className='navbar-brand  fw-bold'>LOGO</Link>
+
+            <button className='navbar-toggler text-light' type="button"  data-bs-target="#nav-collapse" data-bs-toggle="collapse">
+                <span className='navbar-toggler-icon  bg-light'></span>
+            </button>
+
+            <div className='collapse navbar-collapse justify-content-center' id="nav-collapse">
+                <ul className='navbar-nav'>
+                   <li className='nav-item'><NavLink to="/" className='nav-link  'id="menu-item">Home</NavLink></li>
+                   <li className='nav-item'><NavLink to="/about" className='nav-link 'id="menu-item">About</NavLink></li>
+                   <li className='nav-item'><NavLink to="/service" className='nav-link 'id="menu-item">Service</NavLink></li>
+                   <li className='nav-item'><NavLink to="/skills" className='nav-link 'id="menu-item">Skills</NavLink></li>
+                   
+                   <li className='nav-item'><NavLink to="/project" className='nav-link 'id="menu-item">Projects</NavLink></li>
+                   <li className='nav-item'><NavLink to="/contact" className='nav-link 'id="menu-item">Contact us</NavLink></li>
+                </ul>
+            </div>
+        </div>
+       </nav>
+
     </>
 }
 
